83aac8b049397f11ad81e5de84f45d723016b319
jaylen anteneh <jaylenanteneh@gmail.com>
2017-06-17 22:33:29 +0000

    todo tasks

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,6 +2,12 @@ class TodoController < ApplicationController
     def index 
     end
     def show 
-        @todo_description = "Do something you like"
+        todo_id = params[:id]
+        #@todo_description = "Error"
+        if todo_id == '1'
+            @todo_description = "Task 1"
+        elsif todo_id == '2'
+            @todo_description = "Task 2"
+        end
     end
 end
