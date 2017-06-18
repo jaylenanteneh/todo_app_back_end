42647e6c7383f33eca50deb539720c643ada9628
jaylen anteneh <jaylenanteneh@gmail.com>
2017-06-18 22:33:44 +0000

    create reminder button to work

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 get 'todo/index',to:'todo#index'
+get 'todo/new/',to:'todo#new'
+get 'todo/create/',to:'todo#create'
 get 'todo/show/:id',to:'todo#show'
 get 'todo/settings/',to:'todo#settings'
+
 end
